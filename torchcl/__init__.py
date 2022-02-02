751ea15782fb37d1c6292a8095a7cad07fc2dfa2
nader asadi <asadi.nader97@gmail.com>
2022-02-02 16:28:20 -0500

    rebase

---
+++ torchcl/__init__.py
@@ -0,0 +1,9 @@
+from torchcl import config
+from torchcl import data
+from torchcl import losses
+from torchcl import meters
+from torchcl import methods
+from torchcl import models
+from torchcl import utils
+
+__version__ = "0.0.1"
