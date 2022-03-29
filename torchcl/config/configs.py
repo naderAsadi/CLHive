7328eda4735b4ce84892fa53e17d946c5fa4e856
naderasadi <asadi.nader97@gmail.com>
2022-03-29 19:13:42 -0400

    :bulb: hydra config added

--- torchcl/config/configs.py
+++ torchcl/config/configs.py
@@ -1,7 +1,49 @@
 from dataclasses import dataclass
+from hydra.core.config_store import ConfigStore
 from typing import Dict, List, Optional
 
-import torch
+
+@dataclass
+class DataConfig:
+    dataset: str = "cifar10"
+    root: str = "../cl-datasets/data/"
+    n_tasks: int = 5
+    mem_size: int = 0
+    download: Optional[bool] = True
+    smooth: Optional[bool] = False
+    n_workers: Optional[int] = 0
+    transform: Optional[str] = "base"
+
+
+@dataclass
+class MethodConfig:
+    name: str = "finetuning"
+    model: str = "resnet18"
+    nf: int = 32
+    use_snapshots: bool = False
+    save_snapshots: bool = False
+    snapshots_path: str = "./snapshots"
+
+
+@dataclass
+class TrainConfig:
+    scenario: str = "single_head"
+    batch_size: int = 32
+    buffer_batch_size: int = 32
+    n_epochs: int = 1
+    n_iters: int = 1
+    use_augs: bool = False
+
+
+@dataclass
+class EvalConfig:
+    scenario: str = "single_head"
+    batch_size: int = 32
+    probe_eval: bool = False  # instead of keep_training_data
+    cka_eval: bool = False
+    eval_layer: Optional[int] = 4
+    eval_every: Optional[int] = None
+    n_epochs: int = 1
 
 
 @dataclass
@@ -15,51 +57,9 @@ class OptimConfig:
 
 
 @dataclass
-class MethodConfig:
-    name: str = "finetuning"
-    scenario: str = "single_head"
-    model: str = "resnet18"
-    nf: int = 32
-    use_snapshots: bool = False
-    save_snapshots: bool = False
-
-
-@dataclass
-class TrainConfig:
-    batch_size: int = 32
-    buffer_batch_size: int = 32
-    n_epochs: int = 1
-    iters: int = 1
-    use_augs: bool = False
-    mem_size: int = 0
-    optim: OptimConfig = OptimConfig()
-
-
-@dataclass
-class EvalConfig:
-    batch_size: int = 32
-    probe_eval: bool = False # instead of keep_training_data
-    cka_eval: bool = False
-    eval_layer: Optional[int] = 4
-    eval_every: Optional[int] = None
-    n_epochs: int = 1
-    optim: OptimConfig = OptimConfig()
-
-
-@dataclass
-class DataConfig:
-    dataset: str = "cifar10"
-    root: str = "../cl-datasets/data/"
-    n_tasks: int = 5
-    download: Optional[bool] = True
-    smooth: Optional[bool] = False
-    n_workers: Optional[int] = 0
-    transform: Optional[str] = "base"
-
-
-@dataclass
 class Config:
     method: MethodConfig = MethodConfig()
     data: DataConfig = DataConfig()
     train: TrainConfig = TrainConfig()
     eval: EvalConfig = EvalConfig()
+    optim: OptimConfig = OptimConfig()
--- torchcl/config/parser.py
+++ torchcl/config/parser.py
@@ -1,8 +1,14 @@
+import os
 import sys
-
 import hydra
+from hydra.core.config_store import ConfigStore
 from omegaconf import DictConfig
 
+from .configs import Config, MethodConfig, DataConfig, TrainConfig, EvalConfig, OptimConfig
+
+
+cs = ConfigStore.instance()
+cs.store(name="base_config", node=Config)
 
 def config_parser(
     config_path:str, 
@@ -21,6 +27,6 @@ def config_parser(
     """
 
     overrides = sys.argv[1:]
-    with hydra.initialize(config_path=config_path, job_name=job_name):
-        cfg = hydra.compose(config_name=config_name, overrides=overrides)
+    hydra.initialize_config_dir(config_dir=os.path.abspath(config_path), job_name=job_name)
+    cfg = hydra.compose(config_name=config_name, overrides=overrides)
     return cfg
