:bug: transform, head forwarding, label normalization

torchcl/models/model_wrapper.py
@@ -25,7 +25,8 @@ class ModelWrapper(nn.Module):
     def __init__(
         self, 
         model: nn.Module, 
-        heads: nn.ModuleDict
+        heads: nn.ModuleDict,
+        scenario: str = 'single_head',
     ) -> None:
         """[summary]
 
@@ -37,7 +38,12 @@ class ModelWrapper(nn.Module):
         super(ModelWrapper, self).__init__()
         self._model = model
         self._heads = heads
-        self._pred_dim = list(self._heads.values())[0].out_dim
+        self._scenario = scenario
+
+        if self._scenario is 'single_head':
+            self._pred_dim = self._heads.out_dim
+        else:
+            self._pred_dim = list(self._heads.values())[0].out_dim
     
     @classmethod
     def from_config(cls, config: Dict[str, Any], *args, **kwargs) -> "ModelWrapper":
@@ -62,8 +68,8 @@ class ModelWrapper(nn.Module):
         return self._model(x)
 
     def forward_head(self, x, task: int):
-        if isinstance(task, int):
-            return self._heads[str(task)](x)
+        if self._scenario is 'single_head':
+            return self._heads(x)
         
         pred = torch.zeros(x.size(0), self._pred_dim).to(x.get_device())
         tasks = task.unique().tolist()
@@ -76,9 +82,10 @@ class ModelWrapper(nn.Module):
         """
         Perform computation of blocks in the order define in get_blocks.
         """
-        assert task not in self._heads.keys(), (
-            f"{task} does not exist in {self._heads.keys()}"
-        )
+        if self._scenario is not 'single_head':
+            assert task not in self._heads.keys(), (
+                f"{task} does not exist in {self._heads.keys()}"
+            )
 
         x = self.forward_model(x)
         x = self.forward_head(x, task)

torchcl/models/resnet.py
@@ -140,30 +140,30 @@ class ResNet(nn.Module):
 
 @register_model("resnet18")
 class ResNet18(ModelWrapper):
-    def __init__(self, heads: nn.ModuleDict, **kwargs):
+    def __init__(self, heads: nn.ModuleDict, scenario: str, **kwargs):
         model = ResNet(BasicBlock, [2, 2, 2, 2], **kwargs)
-        super().__init__(model=model, heads=heads)
+        super().__init__(model=model, heads=heads, scenario=scenario)
     
 
 @register_model("resnet34")
 class ResNet34(ModelWrapper):
-    def __init__(self, heads: nn.ModuleDict, **kwargs):
+    def __init__(self, heads: nn.ModuleDict, scenario: str, **kwargs):
         model = ResNet(BasicBlock, [3, 4, 6, 3], **kwargs)
-        super().__init__(model=model, heads=heads)
+        super().__init__(model=model, heads=heads, scenario=scenario)
 
 
 @register_model("resnet50")
 class ResNet50(ModelWrapper):
-    def __init__(self, heads: nn.ModuleDict, **kwargs):
+    def __init__(self, heads: nn.ModuleDict, scenario: str, **kwargs):
         model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
-        super().__init__(model=model, heads=heads)
+        super().__init__(model=model, heads=heads, scenario=scenario)
 
 
 @register_model("resnet101")
 class ResNet101(ModelWrapper):
-    def __init__(self, heads: nn.ModuleDict, **kwargs):
+    def __init__(self, heads: nn.ModuleDict, scenario: str, **kwargs):
         model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
-        super().__init__(model=model, heads=heads)
+        super().__init__(model=model, heads=heads, scenario=scenario)
 
 
 # model_dict = {

torchcl/utils/evaluator/probe_evaluator.py
@@ -53,7 +53,7 @@ class ProbeEvaluator(Evaluator):
             self.train_loader.sampler.set_task(task_t, sample_all_seen_tasks=sample_all_seen_tasks)
             optim = torch.optim.SGD(
                 self.linear_heads[str(task_t)].parameters(), 
-                lr=self.config.train.optim.lr,
+                lr=self.config.optim.lr,
                 momentum=0.9,
                 weight_decay=0.
             )