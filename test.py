f9024ef8e5040f635595cb7997103b326b5322af
naderasadi <asadi.nader97@gmail.com>
2022-06-21 11:37:24 -0400

    :bulb: scenarios package and ContinualDataset added

--- test.py
+++ test.py
@@ -6,7 +6,9 @@ dataset = MNISTDataset(root="my/data/path")
 
 scenario = ClassIncremental(dataset=dataset, n_tasks=5, batch_size=32, n_workers=6)
 
-print(f"Number of tasks: {scenario.n_tasks} | Number of classes: {scenario.n_classes}")
+print(
+    f"Number of tasks: {scenario.n_tasks} | Number of classes: {scenario.n_classes}"
+)
 
 for task_id, train_loader in enumerate(scenario):
     for x, y, t in train_loader:
